01c01bbf3a6b9164b1100d5bad5537fb64c1f949
Matthew Byrom <matthew.james.byrom@tamu.edu>
2023-11-27 10:05:50 -0500

    Planning outline for EDA

--- EDA.R
+++ EDA.R
@@ -13,3 +13,17 @@ df = read.csv("valentim_Academic_Success.csv", sep = ";") #Semicolon separator
 custom_view(df)
 
 # set data types of variables, (factor(Categorical), int/num = numerical)
+
+# code to look for NA values
+
+# summary statistics/tables
+
+# outliers (only aplicable to continuous) 
+
+# histograms, box plots, bar charts(cat data)
+
+# contingency tables? 
+
+# Correlation: Crameer's V or phi squared for categorical data (keep in mind most variables are categorical)
+
+
