66ea71c13f536a7bd2d008823a7990be0b35c518
Matthew Byrom <matthew.james.byrom@tamu.edu>
2023-11-27 12:10:39 -0500

    added DS_store to git ignore. Added cat/Continuous variable labeling

--- EDA.R
+++ EDA.R
@@ -13,6 +13,9 @@ df = read.csv("valentim_Academic_Success.csv", sep = ";") #Semicolon separator
 custom_view(df)
 
 # set data types of variables, (factor(Categorical), int/num = numerical)
+df$Marital.status <- factor(df$Marital.status)
+cat_ColNames <- names(df[-c(7, 13, 22:36)]) # age is 20, could be considered ordinal. Includes response variable (Target) 
+continuous_ColNames <- names(df[c(7, 13, 22:36)]) # all of the predictors not in cat_ColNames
 
 # code to look for NA values
 
@@ -25,5 +28,4 @@ custom_view(df)
 # contingency tables? 
 
 # Correlation: Crameer's V or phi squared for categorical data (keep in mind most variables are categorical)
-
-
+# note: haven't used Cramer's V before. But it's cited a bunch of places for categorical data 
