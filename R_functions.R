f638022a023b2e452ffc226b54382c96e68064b1
Matthew Byrom <matthew.james.byrom@tamu.edu>
2023-11-27 16:56:24 -0500

    added more functions for custom plots

--- R_functions.R
+++ R_functions.R
@@ -9,3 +9,23 @@ custom_view = function(df){
     row.names = NULL
   )
 }
+
+
+# histograms, box plots, bar charts(cat data)
+cust_barplots = function(df, colNames){
+  for(i in colNames){
+    # barplots % of observations 
+    barplot(prop.table(table(df[i])), xlab = "categories", ylab = "% observations", main = as.character(i))
+    # barplots with sum counts 
+    print(ggplot(df) + geom_bar(aes_string(x = i)) + ggtitle(as.character(i)) +
+            xlab("categories") +
+            theme(plot.title = element_text(hjust = 0.5)))
+  }
+}
+
+# plots for continuous variables
+cust_hist = function(df, colNames){
+  for(i in colNames){
+    hist(prop.table(table(df[i])), xlab = "numerical value", main = as.character(i), breaks = 10) # change breaks for bins
+  }
+}
